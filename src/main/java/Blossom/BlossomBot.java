5738876924968ab5e306f220c8827dcd8216427f
Laeti-Park <creator98@naver.com>
2022-06-07 12:53:36 +0900

    [Add] token

--- src/main/java/Blossom/BlossomBot.java
+++ src/main/java/Blossom/BlossomBot.java
@@ -9,21 +9,28 @@ import net.dv8tion.jda.api.hooks.EventListener;
 import org.jetbrains.annotations.NotNull;
 
 import javax.security.auth.login.LoginException;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class BlossomBot implements EventListener {
 
     // JDA: Discord API를 자바에서 사용할 수 있는 라이브러리
     public static JDA jda;
 
-    public static void main(String[] args) throws LoginException {
-        String token = "OTgwMDgwODM4OTk5OTMyOTk4.GCnRAQ.QeezxlZ5oCEenPThrRHkP2aKoE1eDW_RMiZyM0";
+    public static void main(String[] args) throws LoginException, IOException {
+
+        BufferedReader tokenFile = new BufferedReader(new FileReader("./token")); // 보안을 위해 토큰 파일로 분리
+
+        String token = tokenFile.readLine();
         jda = JDABuilder.createDefault(token).addEventListeners(new BlossomBot()).build();
         jda.addEventListener(new ChatListener());
     }
 
     @Override
     public void onEvent(@NotNull GenericEvent event) {
-        if(event instanceof ReadyEvent)
+        if (event instanceof ReadyEvent)
             System.out.println("Hello World!");
     }
 }
